8a545d12eb4f713b75b5f19aaba3bb443439d3ac
imwyw <now_way@126.com>
2018-12-17 08:53:24 +0800

    公共帮助类

---
+++ Src/CoSales/trunk/CoSales.Common/Log4Helper.cs
@@ -0,0 +1,49 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoSales.Common
+{
+    /// <summary>
+    /// log4net实例获取
+    /// 单例模式的应用
+    /// </summary>
+    public class Log4Helper
+    {
+        /// <summary>
+        /// lock对象
+        /// </summary>
+        private static object lockLog = new object();
+
+        /// <summary>
+        ///  日志记录器
+        /// </summary>
+        private static ILog _log = null;
+
+        /// <summary>
+        /// 日志记录接口
+        /// </summary>
+        public static ILog Log
+        {
+            get
+            {
+                if (_log == null)
+                {
+                    // 初始化配置，从当前项目下读取log4net.config配置文件
+                    string configFile = AppDomain.CurrentDomain.BaseDirectory + "log4net.config";
+                    log4net.Config.XmlConfigurator.Configure(new FileInfo(configFile));
+
+                    lock (lockLog)
+                    {
+                        _log = LogManager.GetLogger("WebLogger");
+                    }
+                }
+                return _log;
+            }
+        }
+    }
+}
---
+++ Src/CoSales/trunk/CoSales.Common/LogHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoSales.Common
+{
+    /// <summary>
+    /// 日志帮助类
+    /// 单例
+    /// </summary>
+    public class LogHelper
+    {
+        private LogHelper() { }
+
+        static LogHelper logger;
+        static readonly object lockObj = new object();
+
+        //public LogHelper GetInstance()
+        public static LogHelper Logger
+        {
+            get
+            {
+                if (logger == null)
+                {
+                    lock (lockObj)
+                    {
+                        if (logger == null)
+                        {
+                            logger = new LogHelper();
+                        }
+                    }
+                }
+                return logger;
+            }
+        }
+
+        public void WriteLog(string title, string content)
+        {
+            // 日志名称，每天一个记录log文件
+            string logName = DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            string logFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, logName);
+
+            if (!File.Exists(logFile))
+            {
+                File.Create(logFile).Dispose();
+            }
+            using (StreamWriter sw = new StreamWriter(logFile, true, Encoding.UTF8))
+            {
+                sw.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")}----{title}----{content}");
+            }
+        }
+    }
+}
