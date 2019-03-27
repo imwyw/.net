81f6d0a3dd6c90670651b4cf8b245b83baca9958
imwyw <now_way@126.com>
2019-03-27 16:52:07 +0800

    WebApi token 示例

---
+++ Src/HttpServer/WebApiDemo/ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ProductApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductApp.Controllers
+{
+    [Authorize]
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        
+        [HttpPost]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ Src/HttpServer/WebApiDemo/ProductApp/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ProductApp
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            //模拟登陆验证未通过
+            if (!context.UserName.Equals("admin") || !context.Password.Equals("123"))
+            {
+                context.SetError("验证失败", "用户名或密码错误");
+                return;
+            }
+
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+    }
+}
