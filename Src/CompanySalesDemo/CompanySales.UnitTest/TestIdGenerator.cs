032e4222a4065646b1e46264f50499a1da5d19cf
imwyw <now_way@126.com>
2019-12-18 08:56:10 +0800

    增加id生成器单元测试； 统一各项目框架版本；

---
+++ Src/CompanySalesDemo/CompanySales.UnitTest/TestIdGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanySales.UnitTest
+{
+    [TestClass]
+    public class TestIdGenerator
+    {
+        [TestMethod]
+        public void TestId()
+        {
+            List<long> idList = new List<long>();
+            for (int i = 0; i < 10000; i++)
+            {
+                idList.Add(Common.IdGenerator.GeneratorInt64());
+            }
+
+            Assert.IsTrue(idList.Count > 0);
+        }
+    }
+}
