2655c483e434e9e3ed4031c87d277e3b10ff6846
imwyw <now_way@126.com>
2020-04-10 18:38:20 +0800

    导入elementUI 和 axios

--- Src/CompanySales_Speration/companysales_app/src/main.js
+++ Src/CompanySales_Speration/companysales_app/src/main.js
@@ -4,12 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 导入 ElementUI 组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 导入 axios 组件
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
+  // components: { App },
+  // template: '<App/>'
 })
