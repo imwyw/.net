49b580e6a140c86b444623864b8ad0098bff94e3
imwyw <now_way@126.com>
2020-04-11 00:04:43 +0800

    完善登录页

--- Src/CompanySales_Speration/companysales_app/src/views/Login.vue
+++ Src/CompanySales_Speration/companysales_app/src/views/Login.vue
@@ -2,15 +2,16 @@
   <el-container class="fill-container">
     <el-form
       class="login-container"
-      ref="loginForm"
+      ref="refLogin"
       :model="loginForm"
       :label-position="labelPosition"
+      :rules="rules"
       label-width="80px"
     >
-      <el-form-item label="用户名：">
+      <el-form-item label="用户名：" prop="userid">
         <el-input v-model="loginForm.userid" placeholder="用户ID"></el-input>
       </el-form-item>
-      <el-form-item label="密码：">
+      <el-form-item label="密码：" prop="password">
         <el-input type="password" v-model="loginForm.password" placeholder="password"></el-input>
       </el-form-item>
       <el-form-item>
@@ -32,26 +33,45 @@ export default {
         password: ""
       },
       // 表单标签顶部对齐
-      labelPosition: "top"
+      labelPosition: "top",
+      // 表单验证规则
+      rules: {
+        // 规则必须与data属性对应，el-form-item的prop属性保持一致
+        userid: [
+          { required: true, message: "用户名不可为空", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码不可为空", trigger: "blur" },
+          { min: 2, message: "密码太短", trigger: "blur" }
+        ]
+      }
     };
   },
   methods: {
     loginHandler() {
       let sdata = this.$data.loginForm;
-      this.axios
-        .get(`/Security/Login?uid=${sdata.userid}&pwd=${sdata.password}`)
-        .then(res => {
-          // 验证通过
-          if (res.data.status) {
-            // 路由跳转
-            this.$router.push({ name: "Home" });
-          } else {
-            this.$message.error("用户名或密码错误");
-          }
-        })
-        .catch(err => {
-          console.error(err);
-        });
+      // 提交时需要验证规则
+      this.$refs["refLogin"].validate(valid => {
+        // 验证通过方可提交
+        if (valid) {
+          this.axios
+            .get(`/Security/Login?uid=${sdata.userid}&pwd=${sdata.password}`)
+            .then(res => {
+              // 验证通过
+              if (res.data.status) {
+                // 路由跳转
+                this.$router.push({ name: "Home" });
+              } else {
+                this.$message.error("用户名或密码错误");
+              }
+            })
+            .catch(err => {
+              console.error(err);
+            });
+        } else {
+          this.$message.error("信息填写有误！");
+        }
+      });
     }
   }
 };
