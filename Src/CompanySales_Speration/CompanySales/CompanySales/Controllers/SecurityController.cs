bfd70f135b9b8d18f73e980557977f14c864bdd1
imwyw <now_way@126.com>
2020-04-10 17:58:58 +0800

    实现Login api

---
+++ Src/CompanySales_Speration/CompanySales/CompanySales/Controllers/SecurityController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanySales.Model;
+using CompanySales.Repository.Business;
+using CompanySales.Repository.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanySales.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        [HttpGet]
+        public ResultDTO Login(string uid, string pwd)
+        {
+            var dto = new ResultDTO();
+
+            if (null == uid || null == pwd)
+            {
+                return dto;
+            }
+            var res = UserBiz.Login(uid, pwd);
+            dto.Status = res != null;
+            dto.Data = res;
+
+            return dto;
+        }
+    }
+}
---
+++ Src/CompanySales_Speration/CompanySales/CompanySales/Model/ResultDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanySales.Model
+{
+    /// <summary>
+    /// 和前端数据交互的封装
+    /// </summary>
+    public class ResultDTO
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
